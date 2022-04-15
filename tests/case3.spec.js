4f4854b4521e2548a8320dc2ff899186df3cfbde
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-04-15 10:24:49 +0300

    fix

--- tests/case3.spec.js
+++ tests/case3.spec.js
@@ -1,11 +1,12 @@
 const { test, expect, request  } = require('@playwright/test');
 
-const login = {userName: "kh_user_38",password: "@Qwerty123!"};
+const login = {userName: "kh_user_58",password: "@Qwerty123!"};
 
 let userID = "";
 let token = "";
 
-const bookISBN = '978-3-16-148410-0';
+const bookISBN = '9781449325862';
+const newBookISBN = '9781593275846';
 
 test.describe('Case 3', async() => {
 
@@ -29,8 +30,15 @@ test.describe('Case 3', async() => {
         console.log("Authorized: " + responseJsonAut);
         expect(responseAut.ok()).toBeTruthy();
 
-        // Get authorized
-        const responseAutGet = await request.get("https://demoqa.com/Account/v1/User/" + userID);
+        console.log(userID)
+        console.log(token)
+
+        //Get authorized
+        const responseAutGet = await request.get("https://demoqa.com/Account/v1/User/" + userID, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
         console.log(responseAutGet);
         const responseJsonAutGet = await responseAutGet.json();
         console.log("Get authorized: " + responseJsonAutGet);
@@ -38,11 +46,22 @@ test.describe('Case 3', async() => {
     
     });
 
-    test('Add one book to the collection', async ({ request }) => {
+    test('Get books ', async ({ request }) => {
+        const responseAB = await request.get("https://demoqa.com/BookStore/v1/Books", {
+            
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        console.log(responseAB);
+        expect(responseAB.ok()).toBeTruthy();
+    });
+
+    test('Book in the collection', async ({ request }) => {
         const responseAB = await request.post("https://demoqa.com/BookStore/v1/Books", {
             
             headers: {
-                Authorization: `token ${token}`,
+                Authorization: `Bearer ${token}`,
             },
             data: {
                 userId: userID,
@@ -58,17 +77,42 @@ test.describe('Case 3', async() => {
         expect(responseAB.ok()).toBeTruthy();
     });
 
+    test('Put new book to the collection', async ({ request }) => {
+        const responseAB = await request.put("https://demoqa.com/BookStore/v1/Books/" + bookISBN, {
+            
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            data: {
+                userId: userID,
+                isbn: newBookISBN
+            }
+            
+        });
+        console.log(responseAB);
+        expect(responseAB.ok()).toBeTruthy();
+    });
+
     test('Access your profile and check if the added book is displayed there', async ({ request }) => {
 
     });
 
     test('Delete all books', async ({ request }) => {
-
+        const response = await request.delete("https://demoqa.com/BookStore/v1/Books?UserId=" + userID, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        expect(response.ok()).toBeTruthy();
     });
 
     test('Delete account' ,async({request}) => {
-        //const response = await request.delete("https://demoqa.com/Account/v1/User/" + userID);
-        //expect(response.ok()).toBeTruthy();
+        const response = await request.delete("https://demoqa.com/Account/v1/User/" + userID, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        expect(response.ok()).toBeTruthy();
     });
 
 });
